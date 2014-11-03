b2a59d794b942ddf4938ddf642c69fe33253a19f
Calla <callapatel@gmail.com>
2014-11-03 14:50:16 -0800

    omg

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,11 +1,11 @@
 require './lib/dice'
 
 describe Dice do
-  before(:all) do
-    @die = Dice.new
+  subject do
+    Dice.new
   end
 
   specify 'role' do
-    expect @die_face.to_eq ( (/[9JQKA]/).match(Dice.new.die_face)[0] )
+    expect(subject.role).to eq( (/[9JQKA]/).match(Dice.new.die_face)[0] )
   end
 end
